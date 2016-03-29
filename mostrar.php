76e2854e2f8b0f667a4fdb3f61e2960ea76d670e
matiaspro3 <matias9387@hotmail.com>
2016-03-29 06:56:11 -0300

    Signed-off-by: matiaspro3 <matias9387@hotmail.com>

---
+++ mostrar.php
@@ -0,0 +1,13 @@
+<h1>Mostrar Datos Rec.</h1>
+Mostrar php
+<?php 
+
+
+echo "<br> Reci.  por  Post <br>"   ; 
+
+$nombreuser =$_POST["nombre"];
+echo  "Nombre es : ".$nombreuser;
+//var_dump($nombreuser);
+//echo  "get <br>"   ; 
+//var_dump($_GET);
+ ?>
