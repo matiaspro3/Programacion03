1a0afa753d63e456208176a19616a5a325c5a962
matiaspro3 <matias9387@hotmail.com>
2016-03-29 07:21:37 -0300

    Clase 3 .0

---
+++ ELMayor.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title> EL Mayor</title>
+</head>
+<body>
+
+	<form 	method = "post">
+	<input type="text" name ="elPrimero"> 		
+	<input type="text" name ="elSegundo">
+	<input type="text" name ="elTercero">
+	<input type ="submit" value ="Calcular" name="calcular">
+
+
+
+
+
+
+	</form>
+
+</body>
+</html>
--- mostrar.php
+++ mostrar.php
@@ -2,12 +2,23 @@
 Mostrar php
 <?php 
 
-
 echo "<br> Reci.  por  Post <br>"   ; 
 
 $nombreuser =$_POST["nombre"];
-echo  "Nombre es : ".$nombreuser;
+$contrauser =$_POST["contrasenia"];
 //var_dump($nombreuser);
 //echo  "get <br>"   ; 
 //var_dump($_GET);
+if ($nombreuser == "admin" && $contrauser=="1234")
+{
+	//echo  "Nombre es : ".$nombreuser;
+	//echo  "<br> Contrasenia es : ".$contrauser;
+include_once "Logueado.html";
+//include_once "NoLogueado.php"
+
+
+}else { include "NoLogueado.html";}
+
+
+
  ?>
